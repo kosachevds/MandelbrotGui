90fc8d339395d433e7b0634a3cb3475913d508ab
Kosachev Daniil <kos-dan@mail.ru>
2020-10-24 18:20:08 +0300

    Removed 'colorize'

--- MandelbrotSet/MandelbrotSet.fs
+++ MandelbrotSet/MandelbrotSet.fs
@@ -19,12 +19,6 @@ let private mapPixel pixelStep minReal minImag x y =
     let imag = (float y) * pixelStep + minImag
     Complex(real, imag)
 
-let private colorize count =
-    let r = (4 * count) % 255
-    let g = (6 * count) % 255
-    let b = (8 * count) % 255
-    Color.FromArgb(r, g, b)
-
 let fillMatrix pixelStep minReal minImag maxIterations (matrix: int[,]) =
     let countIterations = isMandelbrotPoint maxIterations
     let mapPixelWithScale = mapPixel pixelStep minReal minImag
