29f83f58fa45a06dec6daf50fce42ae3438bff6c
Kosachev Daniil <kos-dan@mail.ru>
2020-11-04 18:31:59 +0300

    Added kernel prototype, impelemented fillMatrix

--- MandelbrotGpu/src/mandelbrot.cpp
+++ MandelbrotGpu/src/mandelbrot.cpp
@@ -8,6 +8,8 @@ struct MandelbrotHandle
     MandelbrotParams params;
 };
 
+__global__ void mandelbrotKernel(MandelbrotParams params, CountsMatrix matrix);
+
 MandelbrotHandle * initMandelbrotHandle(const MandelbrotParams * params, int bytes_count)
 {
     auto h = new MandelbrotHandle;
@@ -20,8 +22,17 @@ MandelbrotHandle * initMandelbrotHandle(const MandelbrotParams * params, int byt
     return h;
 }
 
+void fillMatrix(MandelbrotHandle * handle, CountsMatrix * matrix)
+{
+    // TODO: calculate block_size etc
+    mandelbrotKernel<<<matrix->rows, matrix->columns>>>(handle->params, *matrix);
+    auto bytes_count = matrix->rows * matrix->columns * sizeof(int);
+    cudaMemcpy(matrix->buffer, handle->gpu_buffer, bytes_count, cudaMemcpyDeviceToHost);
+}
+
 void freeMandelbrotHandle(MandelbrotHandle * handle)
 {
     cudaFree(handle->gpu_buffer);
     delete handle;
 }
+
