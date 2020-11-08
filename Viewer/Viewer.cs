9fa83df09367008c5b8b6df74cb9de9c11906d88
Kosachev Daniil <kos-dan@mail.ru>
2020-11-08 16:55:38 +0300

    Remade with field MandelbrotParams

--- Viewer/Viewer.cs
+++ Viewer/Viewer.cs
@@ -9,25 +9,42 @@ namespace Mandelbrot
         private const double ScaleFactor = 0.2;
         private const int MaxIterations = 50;
         private double scaleStep;
-        private double minReal;
-        private double minImag;
-        private double pixelStep;
         private readonly Bitmap bitmap;
         private readonly int[] countsMatrix;  // TODO: with sbyte
         private readonly MandelbrotGpu mandelbrotGpu;
+        private MandelbrotParams mbParams;
 
         private int AreaWidth => this.setView.Width;
         private int AreaHeight => this.setView.Height;
+        private double PixelStep
+        {
+            get => this.mbParams.PixelStep;
+            set => this.mbParams.PixelStep = value;
+        }
+        private double MinReal
+        {
+            get => this.mbParams.MinReal;
+            set => this.mbParams.MinReal = value;
+        }
+        private double MinImag
+        {
+            get => this.mbParams.MinImag;
+            set => this.mbParams.MinImag = value;
+        }
 
         public Viewer()
         {
             // TODO: check MaxValue for matrix item
             InitializeComponent();
 
-            this.minReal = -1.5;
-            this.minImag = -1.2;
-            this.pixelStep = 1 / 175.0;
-            this.scaleStep = ScaleFactor * this.AreaWidth * this.pixelStep;
+            this.mbParams = new MandelbrotParams
+            {
+                MinReal = -1.5,
+                MinImag = -1.2,
+                PixelStep = 1 / 175.0,
+                MaxIteration = MaxIterations
+            };
+            this.scaleStep = ScaleFactor * this.AreaWidth * this.mbParams.PixelStep;
             this.countsMatrix = new int[this.AreaWidth * this.AreaHeight];
             this.bitmap = new Bitmap(this.AreaWidth, this.AreaHeight);
             this.mandelbrotGpu = new MandelbrotGpu(this.AreaHeight, this.AreaWidth);
@@ -37,14 +54,7 @@ namespace Mandelbrot
 
         private void DrawSet()
         {
-            var mparams = new MandelbrotParams
-            {
-                PixelStep = this.pixelStep,
-                MinReal = this.minReal,
-                MinImag = this.minImag,
-                MaxIteration = MaxIterations
-            };
-            mandelbrotGpu.FillMatrix(ref mparams, this.countsMatrix);
+            mandelbrotGpu.FillMatrix(ref this.mbParams, this.countsMatrix);
             CountsToBitmap(this.countsMatrix, this.bitmap);
             this.setView.Refresh();
         }
@@ -76,42 +86,42 @@ namespace Mandelbrot
 
         private void ZoomIn()
         {
-            this.scaleStep = this.AreaWidth * this.pixelStep * ScaleFactor;
+            this.scaleStep = this.AreaWidth * this.PixelStep * ScaleFactor;
             Scale(-this.scaleStep);
         }
 
         private void ZoomOut()
         {
             Scale(this.scaleStep);
-            this.scaleStep = this.AreaWidth * this.pixelStep * ScaleFactor /
+            this.scaleStep = this.AreaWidth * this.PixelStep * ScaleFactor /
                 (1 - 2 * ScaleFactor);
         }
 
         private void Scale(double value)
         {
-            this.minReal -= value;
-            this.minImag -= value;
-            this.pixelStep += 2 * value / this.AreaWidth;
+            this.MinReal -= value;
+            this.MinImag -= value;
+            this.PixelStep += 2 * value / this.AreaWidth;
         }
 
         private void MoveUp()
         {
-            this.minImag -= this.scaleStep;
+            this.MinImag -= this.scaleStep;
         }
 
         private void MoveDown()
         {
-            this.minImag += this.scaleStep;
+            this.MinImag += this.scaleStep;
         }
 
         private void MoveLeft()
         {
-            this.minReal -= this.scaleStep;
+            this.MinReal -= this.scaleStep;
         }
 
         private void MoveRight()
         {
-            this.minReal += this.scaleStep;
+            this.MinReal += this.scaleStep;
         }
 
         private void RedrawWith(Action action)
