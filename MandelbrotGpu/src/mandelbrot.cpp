dc72222b09c583a54234ec7dc6aa7b480c9b0bd5
Kosachev Daniil <kos-dan@mail.ru>
2020-11-08 16:15:02 +0300

    Renames

--- MandelbrotGpu/src/mandelbrot.cpp
+++ MandelbrotGpu/src/mandelbrot.cpp
@@ -19,7 +19,7 @@ struct MandelbrotHandle
 };
 
 __global__ void mandelbrotKernel(MandelbrotParams params, int rows, int columns, int* buffer);
-__device__ inline cuDoubleComplex mapPixel(double pixel_step, double min_real, double min_imag, int i, int j);
+__device__ inline cuDoubleComplex mapPixel(double pixel_step, double min_real, double min_imag, int x, int y);
 static int ceilDivision(int value, int divider);
 
 MandelbrotHandle * initMandelbrotHandle(int rows, int columns)
@@ -62,28 +62,28 @@ void freeMandelbrotHandle(MandelbrotHandle * handle)
 
 __global__ void mandelbrotKernel(MandelbrotParams params, int rows, int columns, int* buffer)
 {
-    auto i = blockIdx.y * blockDim.y + threadIdx.y;
-    auto j = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i >= rows || j >= columns) {
+    auto y = blockIdx.y * blockDim.y + threadIdx.y;
+    auto x = blockIdx.x * blockDim.x + threadIdx.x;
+    if (y >= rows || x >= columns) {
         return;
     }
 
-    auto add = mapPixel(params.pixel_step, params.min_real, params.min_imag, j, i);
+    auto add = mapPixel(params.pixel_step, params.min_real, params.min_imag, x, y);
     cuDoubleComplex z = make_cuDoubleComplex(0.0, 0.0);
     int count = 0;
     while (count < params.max_iteration && cuCabs(z) < ABS_LIMIT) {
         z = cuCadd(cuCmul(z, z), add);
         ++count;
     }
-    auto flat_index = i * columns + j;
+    auto flat_index = y * columns + x;
     buffer[flat_index] = count;
 }
 
-__device__ cuDoubleComplex mapPixel(double pixel_step, double min_real, double min_imag, int i, int j)
+__device__ cuDoubleComplex mapPixel(double pixel_step, double min_real, double min_imag, int x, int y)
 {
     return make_cuDoubleComplex(
-        min_real + i * pixel_step,
-        min_imag + j * pixel_step
+        min_real + x * pixel_step,
+        min_imag + y * pixel_step
     );
 }
 
